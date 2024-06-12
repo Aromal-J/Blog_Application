5fc34d3bd902a005fe34450c81c84b3487b1b73e
Aromal J <aaromalj0355@gmail.com>
2024-06-12 23:47:18 +0530

    text editor context and editor state

--- client/src/components/blog-editor.component.jsx
+++ client/src/components/blog-editor.component.jsx
@@ -6,7 +6,7 @@ import { uploadImage } from "../common/aws";
 import { useContext, useEffect, useRef } from "react";
 import { Toaster, toast } from "react-hot-toast";
 import { EditorContext } from "../pages/editor.pages";
-import EditorJS from '@editorjs/editorjs'
+import EditorJS from "@editorjs/editorjs";
 import { tools } from "./tools.component";
 
 const BlogEditor = () => {
@@ -16,20 +16,22 @@ const BlogEditor = () => {
     setBlog,
     editorState,
     setEditorState,
+    textEditor,
+    setTextEditor,
   } = useContext(EditorContext);
 
-
-  useEffect(()=>{
-    let editor = new EditorJS({
-      holder: 'textEditor',
-      data: '',
-      tools: tools,
-      placeholder: "Let's write an awesome story"
-    })
-  },[])
+  useEffect(() => {
+    setTextEditor(
+      new EditorJS({
+        holder: "textEditor",
+        data: "",
+        tools: tools,
+        placeholder: "Let's write an awesome story",
+      })
+    );
+  }, []);
 
   const handleBannerUpload = (e) => {
-
     let img = e.target.files[0];
     // console.log(img);
 
@@ -38,11 +40,10 @@ const BlogEditor = () => {
       uploadImage(img)
         .then((url) => {
           if (url) {
-
             toast.dismiss(loadingToast);
             toast.success("Uploaded");
 
-            setBlog({...blog, banner:url})
+            setBlog({ ...blog, banner: url });
           }
         })
         .catch((err) => {
@@ -67,10 +68,35 @@ const BlogEditor = () => {
     setBlog({ ...blog, title: input.value });
   };
 
-  const handleBannerError=(e)=>{
-    let img = e.target
-    console.log(img);
-    img.src = defaultBanner
+  const handleBannerError = (e) => {
+    let img = e.target;
+    // console.log(img);
+    img.src = defaultBanner;
+  };
+
+  const handlePublishEvent=()=>{
+
+    if(!banner.length){
+      return toast.error("Please upload a blog banner to publish it")
+    }
+
+    if(!title.length){
+      return toast.error("Write blog title to publish it")
+    }
+
+    if(textEditor.isReady){
+      textEditor.save().then(data=>{
+       if(data.blocks.length){
+        setBlog({...blog, content:data})
+        setEditorState("Publish")
+       }else{
+        return toast.error("Write something in your blog to publish")
+       }
+      }).catch(err=>{
+        console.log(err);
+      })
+    }
+
   }
 
   return (
@@ -79,10 +105,12 @@ const BlogEditor = () => {
         <Link to="/" className="flex-none w-10">
           <img src={logo} />
         </Link>
-        <p className="max-md:hidden text-black line-clamp-1 w-full">{title.length?title:"New Blog"}</p>
+        <p className="max-md:hidden text-black line-clamp-1 w-full">
+          {title.length ? title : "New Blog"}
+        </p>
 
         <div className="flex gap-4 ml-auto">
-          <button className="btn-dark py-2 ">Publish</button>
+          <button className="btn-dark py-2 " onClick={handlePublishEvent}>Publish</button>
           <button className="btn-light py-2">Save Draft</button>
         </div>
       </nav>
@@ -92,7 +120,11 @@ const BlogEditor = () => {
           <div className="mx-auto max-w-[900px] w-full">
             <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
               <label htmlFor="uploadBanner">
-                <img src={banner} className="z-20" onError={handleBannerError} />
+                <img
+                  src={banner}
+                  className="z-20"
+                  onError={handleBannerError}
+                />
                 <input
                   id="uploadBanner"
                   type="file"
@@ -109,7 +141,7 @@ const BlogEditor = () => {
               onChange={handleTitleChange}
             ></textarea>
 
-            <hr className="w-full opacity-10 my-5"/>
+            <hr className="w-full opacity-10 my-5" />
 
             <div id="textEditor" className="font-gelasio"></div>
           </div>
