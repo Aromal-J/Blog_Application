abb81165f7563a9fef2619f82a9489e24fd0a5d1
Aromal J <aaromalj0355@gmail.com>
2024-08-13 01:37:50 +0530

    trending blog router-server

--- server/server.js
+++ server/server.js
@@ -177,7 +177,7 @@ server.get("/latest-blogs", (req, res) => {
       "author",
       "personal_info.profile_img personal_info.username personal_info.fullname -_id"
     )
-    .sort({ publishedAt: -1 })
+    .sort({ "publishedAt": -1 })
     .select("blog_id title des tags banner activity publishedAt -_id")
     .limit(maxLimit)
     .then((blogs) => {
@@ -187,6 +187,24 @@ server.get("/latest-blogs", (req, res) => {
       return res.status(500).json({ error: err.message });
     });
 });
+
+server.get('/trending-blogs',async(req,res)=>{
+  try {
+    let blog= await Blog.find({ draft: false })
+    .populate(
+      "author",
+      "personal_info.profile_img personal_info.username personal_info.fullname -_id"
+    )
+    .sort({ "activity.total_read":-1,"activity.total_likes":-1 ,"publishedAt": -1 })
+    .select("blog_id title publishedAt -_id")
+    .limit(5)
+
+    return res.status(200).json({blog})
+
+  } catch (err) {
+    return res.status(500).json({error: err.message})
+  }
+})
 
 server.post("/create-blog", verifyJWT, (req, res) => {
   let authorId = req.user;
