21645e45affc653ecf2f5f9abc46eec0452a0493
Aromal J <aaromalj0355@gmail.com>
2024-08-12 23:38:22 +0530

    Home page "Blog card"

--- client/src/pages/home.page.jsx
+++ client/src/pages/home.page.jsx
@@ -4,41 +4,51 @@ import InPageNavigation from "../components/inpage-navigation.component";
 import axios from "axios";
 import { useEffect } from "react";
 import Loader from "../components/loader.component";
+import BlogPostCard from "../components/blog-post.component";
 
 const HomePage = () => {
+  let [blogs, setBlogs] = useState(null);
 
-    let [blogs, setBlogs]= useState(null)
-
-    const fetchLatestBlogs=async()=>{
-        try {
-          let {data:{blogs}} = await axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
-          setBlogs(blogs)
-        } catch (err) {
-            console.log(err);
-        }
+  const fetchLatestBlogs = async () => {
+    try {
+      let {
+        data: { blogs },
+      } = await axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs");
+      setBlogs(blogs);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    useEffect(()=>{
-        fetchLatestBlogs()
-    },[])
+  useEffect(() => {
+    fetchLatestBlogs();
+  }, []);
 
   return (
     <AnimationWrapper>
       <section className="h-cover flex justify-center gap-10">
-
         {/* latest blog */}
         <div className="w-full">
           <InPageNavigation
-            routes={["home", "trending blogs"]} defaultHidden={["trending blogs"]}
+            routes={["home", "trending blogs"]}
+            defaultHidden={["trending blogs"]}
           >
             <>
-
-                {
-                    blogs===null?<Loader/>: blogs.map((blog, i)=>{
-                        return <h1 key={i}>{blog.title}</h1>
-                    })
-                }
-            
+              {blogs === null ? (
+                <Loader />
+              ) : (
+                blogs.map((blog, i) => {
+                  return (
+                    <AnimationWrapper
+                      key={i}
+                      transition={{ duration: 1, delay: i * 0.1 }}
+                      
+                    >
+                      <BlogPostCard content={blog} author={blog.author.personal_info}/>
+                    </AnimationWrapper>
+                  );
+                })
+              )}
             </>
             <h1>Trending blogs here</h1>
           </InPageNavigation>
