d67361479ae549c5646e246c6c6e3776c0b1264d
Aromal J <aaromalj0355@gmail.com>
2024-08-13 17:12:52 +0530

    Home page - filter and trending blogs

--- client/src/pages/home.page.jsx
+++ client/src/pages/home.page.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import AnimationWrapper from "../common/page-animation";
-import InPageNavigation from "../components/inpage-navigation.component";
+import InPageNavigation, { activeTabRef } from "../components/inpage-navigation.component";
 import axios from "axios";
 import { useEffect } from "react";
 import Loader from "../components/loader.component";
@@ -10,6 +10,17 @@ import MinimalBlogPost from "../components/nobanner-blog-post.component";
 const HomePage = () => {
   let [blogs, setBlogs] = useState(null);
   let [trendingBlogs, setTrendingBlogs] = useState(null);
+  let [pageState, setPageState] = useState("home");
+
+  let categories = [
+    "programming",
+    "hollywood",
+    "film making",
+    "cooking",
+    "tech",
+    "social media",
+    "finances",
+  ];
 
   const fetchLatestBlogs = async () => {
     try {
@@ -29,17 +40,36 @@ const HomePage = () => {
       } = await axios.get(
         import.meta.env.VITE_SERVER_DOMAIN + "/trending-blogs"
       );
-      console.log(blogs);
       setTrendingBlogs(blogs);
     } catch (err) {
       console.log(err);
     }
   };
 
+  const loadBlogByCategory = (e) => {
+    let category = e.target.innerText.toLowerCase();
+    setBlogs(null);
+
+    if (pageState == category) {
+      setPageState("home");
+      return;
+    }
+
+    setPageState(category);
+  };
+
   useEffect(() => {
-    fetchLatestBlogs();
-    fetchTrendingtBlogs();
-  }, []);
+
+    activeTabRef.current.click()
+
+    if (pageState === "home") {
+      fetchLatestBlogs();
+    }
+
+    if (!trendingBlogs) {
+      fetchTrendingtBlogs();
+    }
+  }, [pageState]);
 
   return (
     <AnimationWrapper>
@@ -47,7 +77,7 @@ const HomePage = () => {
         {/* latest blog */}
         <div className="w-full">
           <InPageNavigation
-            routes={["home", "trending blogs"]}
+            routes={[pageState, "trending blogs"]}
             defaultHidden={["trending blogs"]}
           >
             <>
@@ -79,7 +109,7 @@ const HomePage = () => {
                     key={i}
                     transition={{ duration: 1, delay: i * 0.1 }}
                   >
-                    <MinimalBlogPost blog={blog} index={i}/>
+                    <MinimalBlogPost blog={blog} index={i} />
                   </AnimationWrapper>
                 );
               })
@@ -88,7 +118,51 @@ const HomePage = () => {
         </div>
 
         {/* filters and trending blog */}
-        <div></div>
+        <div className="min-w-[40%] lg:min-w-[400px] max-w-min border-l border-grey pl-8 pt-5 max-md:hidden">
+          <div className="flex flex-col gap-10">
+            <div>
+              <h1 className="font-medium text-xl mb-8">
+                Stories from all interests
+              </h1>
+
+              <div className="flex gap-3 flex-wrap">
+                {categories.map((category, i) => {
+                  return (
+                    <button
+                      key={i}
+                      className={
+                        "tag " +
+                        (pageState == category ? "bg-black text-white" : " ")
+                      }
+                      onClick={loadBlogByCategory}
+                    >
+                      {category}
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+
+            <h1 className="font-medium text-xl mb-8">
+              Trending <i className="fi fi-rr-arrow-trend-up"></i>
+            </h1>
+
+            {trendingBlogs === null ? (
+              <Loader />
+            ) : (
+              trendingBlogs.map((blog, i) => {
+                return (
+                  <AnimationWrapper
+                    key={i}
+                    transition={{ duration: 1, delay: i * 0.1 }}
+                  >
+                    <MinimalBlogPost blog={blog} index={i} />
+                  </AnimationWrapper>
+                );
+              })
+            )}
+          </div>
+        </div>
       </section>
     </AnimationWrapper>
   );
